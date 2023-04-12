9ce9220068c94d8c9937ba7272a0460d73650aef
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-04-12 09:47:53 +0530

    clientSide

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
+import "./style.css";
 
-const socket = io("ws://localhost:8000");
+// const socket = io("ws://localhost:8000");
+const socket = io("http://192.168.251.222:8000");
 
 function App() {
   const [messages, setMessages] = useState([]);
   const [inputText, setInputText] = useState("");
+  const [name, setName] = useState("");
 
   useEffect(() => {
     socket.on("message", (text) => {
@@ -22,19 +25,45 @@ function App() {
     setInputText("");
   };
 
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleNameClick = () => {
+    socket.emit("login", name);
+  };
+
   return (
-    <div>
-      <ul>
-        {messages.map((message, index) => (
-          <li key={index}>{message}</li>
-        ))}
-      </ul>
-      <input
-        placeholder="message"
-        value={inputText}
-        onChange={handleInputTextChange}
-      />
-      <button onClick={handleSendClick}>Send</button>
+    <div class="chat-container">
+      <div class="chat-box">
+        <ul class="chat-messages">
+          {messages.map((message, index) => (
+            <li key={index} class="chat-message">
+              {message}
+            </li>
+          ))}
+        </ul>
+
+        <div class="chat-input">
+          <input
+            type="text"
+            placeholder="Enter username"
+            value={name}
+            onChange={handleNameChange}
+          />
+          <button onClick={handleNameClick}>Login</button>
+        </div>
+
+        <div class="chat-input">
+          <input
+            type="text"
+            placeholder="Enter message"
+            value={inputText}
+            onChange={handleInputTextChange}
+          />
+          <button onClick={handleSendClick}>Send</button>
+        </div>
+      </div>
     </div>
   );
 }
